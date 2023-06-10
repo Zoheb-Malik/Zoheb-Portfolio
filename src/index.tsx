9f92c70415c5235d68fd5b329ce7f5a1d96f1061
Zoheb-Malik <mzohebm@gmail.com>
2023-06-10 20:37:21 +0100

    fix basenaem

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.render(
     <React.StrictMode>
-      <Router basename='/'>
+      <Router basename='/Zoheb-Portfolio'>
         <App />
       </Router>
     </React.StrictMode>,
