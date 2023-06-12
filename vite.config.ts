aa7a6cecbb0cbde5db79b279c19f07980dc0273b
Zoheb-Malik <mzohebm@gmail.com>
2023-06-12 18:00:23 +0100

    add: hash-routing and change basename

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const __dirname = './public';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Zoheb-Portfolio/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: `${__dirname}/index.html`,
+        404: `${__dirname}/404.html`,
+      },
+    },
+  },
 })
