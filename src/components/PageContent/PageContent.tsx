f29aa101acc7f15f7c47329d1af2ffba3e34e275
Zoheb-Malik <mzohebm@gmail.com>
2023-06-21 01:14:38 +0100

    PageContent.tsx: fix: Easter egg #1.

--- src/components/PageContent/PageContent.tsx
+++ src/components/PageContent/PageContent.tsx
@@ -1,35 +1,34 @@
 import { useState } from 'react';
 import IPageContent from './IPageContent';
 
-export default function PageContent({ title, className, removeMarginTop, children }: IPageContent) {
+export default function PageContent({ title, className = '', removeMarginTop, children }: IPageContent) {
   const [clickCount, setClickCount] = useState(0);
 
-  function sparkleEasterEgg() {
-    const sparkleContainer = document.querySelector('.js-easter-egg');
-    const sparkleContainerContent = (sparkleContainer && sparkleContainer.children[0]) || null;
+  function sparkleEasterEgg({ currentTarget }: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    const content = currentTarget?.children?.[0];
     const newClickCount = clickCount + 1;
 
     if (newClickCount <= 5) {
       setClickCount(newClickCount);
-      sparkleContainer?.classList.add('shake');
+      currentTarget?.classList.add('shake');
       setTimeout(() => {
-        sparkleContainer?.classList.remove('shake');
+        currentTarget?.classList.remove('shake');
       }, 500);
 
       if (newClickCount === 5) {
-        sparkleContainerContent?.classList.add('cursor-default');
-        sparkleContainer?.classList.replace('shake', 'sparkle');
+        content?.classList.add('cursor-default');
+        currentTarget?.classList.replace('shake', 'sparkle');
       }
     }
   }
 
   return (
     <section className='page__section'>
-      {/* <!-- Find the hidden easter egg! (#1) */}
-      <div className='js-easter-egg' onClick={sparkleEasterEgg}>
+      {/* Find the hidden easter egg! (#1) */}
+      <div className='aster-egg--sparkle' onClick={sparkleEasterEgg}>
         <h2 className={removeMarginTop ? 'margin--remove-top' : ''}>{title}</h2>
       </div>
-      <section className={`page__section--content${className ? ` ${className}` : ''}`}>
+      <section className={`page__section--content ${className}`.trim()}>
         {children}
       </section>
     </section>
