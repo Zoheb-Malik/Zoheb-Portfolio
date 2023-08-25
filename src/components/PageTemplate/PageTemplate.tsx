d6e3b7a6c9c3ff9c3b25d994f7c81228c12d3273
Zoheb-Malik <mzohebm@gmail.com>
2023-08-25 16:54:21 +0100

    revert: scroll changes

--- src/components/PageTemplate/PageTemplate.tsx
+++ src/components/PageTemplate/PageTemplate.tsx
@@ -13,7 +13,7 @@ export default function PageTemplate({ header, displayProfile, children }: IPage
         {displayProfile && (
           <section className="margin--add-bottom">
             <a href="https://www.linkedin.com/in/zoheb-malik/" target="_blank">
-              <img className="profile-picture" src="https://i.imgur.com/ex3N1GF.jpeg" alt="Profile Picture" />
+              <img className="profile__picture" src="https://i.imgur.com/ex3N1GF.jpeg" alt="Profile Picture" />
             </a>
           </section>
         )}
